16d32d212fdec2a1b9591acca4a48981c87328aa
wasshoyy <496pitag@gmail.com>
2026-08-21 17:36:11 +0900

    ライントレースの精度向上とディスプレイに笑顔表示

--- delivery_robot/src/spike/linetrace.py
+++ delivery_robot/src/spike/linetrace.py
@@ -8,29 +8,36 @@ motor_right = Motor(Port.E)
 color_sesor = ColorSensor(Port.B)
 ultrasonic_sensor = UltrasonicSensor(Port.C)
 
-p_gain = 12
+p_gain = 5
 # d_gain = 50
-brake_gain = 3
+brake_gain = 20
+brake_delta = 0.05
 speed = 0
 mileage = 0.0
-ref_target = 40
+ref_target = 30
 
 speed_max = 300
+running = False
 
 async def accel(sgn):
-    global speed
+    global speed, running
     acc_time = 100
     i = 0
+    if sgn == 1:
+        running = True
     while i < acc_time:
         await wait(10)
         speed += sgn * (speed_max / acc_time)
         i += 1
-    if sgn == 1: speed = speed_max
-    if sgn == -1: speed = 0
+    if sgn == 1:
+        speed = speed_max
+    if sgn == -1:
+        speed = 0
+        running = False
 
 distance_max = 200
 distance_min = 60
-course_length = 5300
+course_length = 4600
 
 async def check_front_distance():
     global speed
@@ -50,20 +57,27 @@ async def linetrace():
     global mileage
     mileage = 0
     ref_old = 0
-    while speed == 0:
+    brake_now = 0
+    while running == False:
         await wait(10)
-    while speed != 0:
+    while running == True:
         await wait(0.5)
-        ref_now = (await color_sesor.reflection()) - ref_target
-        ref_diff = ref_now - ref_old
+
+        # hsvのvを反射率として用いる
+        hsv = await color_sesor.hsv(surface=True)
+        reflection = hsv.v
+
+        ref_now = reflection - ref_target
+        # ref_diff = ref_now - ref_old
 
         pwm = p_gain * ref_now
         # pwm += d_gain * ref_diff
 
-        brake = abs(brake_gain * ref_now)
+        brake = brake_gain * abs(ref_now)
+        brake_now += max(- brake_delta, min(brake_delta, brake - brake_now))
 
-        left_speed = int(max(-30, speed - brake + pwm))
-        right_speed = int(max(-30, speed - brake - pwm))
+        left_speed = int(max(0, speed - brake_now + pwm))
+        right_speed = int(max(0, speed - brake_now - pwm))
         mileage += (left_speed + right_speed) / (2 * 2 * 1000)
         
         motor_left.run(left_speed)
